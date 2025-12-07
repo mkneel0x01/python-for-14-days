6bf8b59423099fe68c2a7430bdcdac4110595480
mkneel0x01 <mkneel101@gmail.com>
2025-12-08 02:28:43 +0600

    variables

---
+++ Day 1/variable.py
@@ -0,0 +1,21 @@
+#x = 5
+#y = "mkneel"
+#print(x)
+#print(y)
+
+#x = 4       
+#x = "neel" 
+#print(x)
+
+#x = str(7)    # x will be '7'
+#y = int(7)    # y will be 7
+#z = float(7)  # z will be 7.0
+
+#print(x)
+#print(y)
+#print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
